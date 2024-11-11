4e3c3d5d6d4021265fe579d364f4b0172bf464bc
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-11-11 15:12:06 +0530

    fix page search-bar home page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,6 +4,10 @@ import logo from "/logo.png"
 // import { Link } from "react-router-dom";
 
 const Navbar=()=>{
+  const [menubar,setmenubar]=useState(false);
+  const handleclick=()=>{
+    setmenubar(!menubar);
+  }
   return (
     <nav>
      <div className="left">
@@ -19,6 +23,17 @@ const Navbar=()=>{
      <div className="right">
       <a href="/">sigh in</a>
       <a href="/" className="register">sigh up</a>
+      <div className="menuIcon" >
+       <img src="/menu.png" alt="menu" onClick={handleclick} />
+      </div>
+     <div className={menubar?"menu active":"menu"}>
+      <a href="/">Home</a>
+      <a href="/">about</a>
+      <a href="/">Contact</a>
+      <a href="/">Agents</a>
+      <a href="/">sign in</a>
+      <a href="/">sign up</a>
+      </div>
      </div>
     </nav>
   )
