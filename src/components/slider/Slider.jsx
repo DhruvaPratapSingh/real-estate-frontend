7115dd9baf3b35f9787c2f69e509a577052d372d
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-11-12 14:44:55 +0530

    feat add: slider update singlepage

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react"
 import "./slider.scss"
 
-const Slider = () => {
+const Slider = ({images}) => {
+  const [imgidx,setimgidx]=useState(null);
   return (
-    <div>
-      
+    <div className="slider">
+   {imgidx!==null && ( <div className="fullSlider">
+      <div className="arrow">
+        <img src="/arrow.png" alt="" onClick={()=>setimgidx(imgidx >= 1 ? imgidx - 1 : 0)}/>
+      </div>
+        <div className="imgContainer">
+          <img src={images[imgidx]} alt="" />
+        </div>
+        <div className="arrow">
+        <img src="/arrow.png" alt="" className="right" onClick={()=>setimgidx(imgidx <images.length-1 ? imgidx + 1 : 0)}/>
+        </div>
+        <div className="close" onClick={()=>setimgidx(null)}>X</div>
+    </div>)}
+      <div className="bigImage">
+        <img src={images[0]} alt="" onClick={()=>{setimgidx(0)}}/>
+      </div>
+      <div className="smallImages">
+        {
+          images.slice(1).map((image,index)=>(
+            <img src={image} alt="" key={index} onClick={()=>{setimgidx(index+1)}}/>
+          ))
+        }
+      </div>
     </div>
   )
 }
